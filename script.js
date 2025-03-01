ebbc6180cb73474ca9704a72498eb6497eb6cf68
Carson Smith <csm2356@wgu.edu>
2025-03-01 17:35:50 -0600

    added variables and functions to script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+const url = "https://api.weatherbit.io/v2.0/current";
+const apiKey= "8b660c728e074ec0bb58d80f1bfafca7";
+const inputField = document.getElementById("city");
+const submitBtn = document.getElementById("submit");
+const cityDisplay = document.getElementById("cityName");
+const dateDisplay = document.getElementById("date");
+const iconDisplay = document.getElementById("icon");
+const tempDisplay = document.getElementById("temp");
+const cloudDisplay = document.getElementById("cloudCover");
+const windDisplay = document.getElementById("windSpeed");
+
+document.addEventListener("DOMContentLoaded", domContentLoaded);
+
+function domContentLoaded() {
+
+}
+
+function getCityName() {
+
+}
+
+function getWeather() {
+
+}
+
+function displayWeather() {
+
+}
