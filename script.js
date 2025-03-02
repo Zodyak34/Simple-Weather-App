c0412ee4ac5c5a699995b7911b144a35784afcd4
Carson Smith <csm2356@wgu.edu>
2025-03-01 18:46:41 -0600

    added stylesheet and script to html. completed functions to get and display weather data

--- script.js
+++ script.js
@@ -9,20 +9,51 @@ const tempDisplay = document.getElementById("temp");
 const cloudDisplay = document.getElementById("cloudCover");
 const windDisplay = document.getElementById("windSpeed");
 
-document.addEventListener("DOMContentLoaded", domContentLoaded);
+window.addEventListener("DOMContentLoaded", loadedHandler);
 
-function domContentLoaded() {
-
+//adds event listener for submit button when page is loaded
+function loadedHandler() {
+    submitBtn.addEventListener("click", getCityName);
+    console.log("loaded")
 }
 
+//retrieves the city name when submit button is clicked and uses it to call the api
 function getCityName() {
-
+    let cityName = inputField.value;
+    //checks to see if the user has entered a city
+    if (cityName === "") {
+        alert("Please enter a valid city name");
+        return;
+    }
+    //call api
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", getWeather);
+    xhr.open("GET", `${url}?city=${cityName}&key=${apiKey}&units=I`);
+    xhr.responseType = "json";
+    xhr.send();
 }
 
+//takes the api response and uses it to call display weather
 function getWeather() {
+    //checks to make sure the api is giving a response
+    if (this.status !== 200) {
+        alert(`Error Making HTTP request: ${this.statusText}`);
+        return;
+    }
 
+    let response = this.response;
+    let data = response.data[0];
+    console.log(data.weather.icon)
+
+    displayWeather(data);
 }
 
-function displayWeather() {
-
+//uses api response to display current weather data on the webpage
+function displayWeather(weatherData) {
+    cityDisplay.innerText = weatherData.city_name;
+    dateDisplay.innerText = weatherData.ob_time;
+    iconDisplay.src = `https://www.weatherbit.io/static/img/icons/${weatherData.weather.icon}.png`
+    tempDisplay.innerText = `${weatherData.temp}\u00B0F`
+    cloudDisplay.innerText = `${weatherData.clouds}% covered`
+    windDisplay.innerText = `Winds ${weatherData.wind_cdir} ${weatherData.wind_spd} mph`
 }
